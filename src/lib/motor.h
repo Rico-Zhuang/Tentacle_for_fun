71b434c91dc10ac4d44221063064a0611ec6a7aa
Rico <zzhuan13@seas.upenn.edu>
2025-11-24 16:01:34 -0500

    adding control for the third motor

--- src/lib/motor.h
+++ src/lib/motor.h
@@ -7,6 +7,16 @@
 #define MOTOR_1_DDR DDRB
 #define MOTOR_1_PIN PB1
 
+#define MOTOR_2_DDR DDRD
+#define MOTOR_2_PIN PD0
+
+#define MOTOR_3_DDR DDRD
+#define MOTOR_3_PIN PD1
+
+#define MOTOR_1_OCR OCR1A
+#define MOTOR_2_OCR OCR3A
+#define MOTOR_3_OCR OCR4A
+
 // Servo Pulse Width Definitions (in Timer Ticks)
 // Calculation: (Target Time / Prescaler Time)
 // With 16MHz clock and Prescaler 8: 1 Tick = 0.5us
@@ -17,5 +27,7 @@
 
 void servo_init();
 void servo_write(uint8_t angle);
+void servo2_write(uint8_t angle);
+void servo3_write(uint8_t angle);
 
 #endif /* MOTOR_H_ */
